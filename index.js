14d46be87ba72dab12db248be8110bfacec12feb
Mugil6 <mugilan98mg@outlook.com>
2025-02-07 11:36:25 +0530

    Update this file to connect to MySQL database on VM1

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const port = 3300;
+
+const connection = mysql.createConnection({
+  host: '192.168.1.12',
+  user: 'mugil'@'6',
+  password: 'Lang@999',
+  database: 'mugil_dbs'
+ });
+ connection.connect((err) => {
+   if (err) {
+     console.error('Error while connecting to the db:', err.stack);
+     return;
+   }
+   console.log('Successfully connected!');
+ });
+ 
+ app.get('/Name', (req, res) => {
+   connection.query('SELECT * FROM Name', (err, results, fields) => { 
+  if (err) {
+    res.status(500).send('Error in connection');
+    return;
+   }
+   res.json(results);
+  });
+ });
+ 
+app.listen(port, () => {
+  console.log('I am listening at http://localhost:${port}');
+});
+ 
+
